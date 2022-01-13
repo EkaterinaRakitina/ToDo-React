1df570059c3f0272f3974c09fdde0bbfb94183df
EkaterinaRakitina <erakitina.exceedteam@gmail.com>
2022-01-13 18:36:38 +0300

    create add function

--- src/App.js
+++ src/App.js
@@ -5,24 +5,40 @@ import './App.scss';
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
+  const [text, setText] = useState('');
 
-  useEffect(async() => {
-    await axios.get('http://localhost:8000/allTasks').then(res => {
-      setTasks(res.data.data);
+  useEffect(async () => {
+    await axios.get('http://localhost:8000/allTasks').then((res) => {
+      // setTasks(res.data.data);
+      console.log(res.data.data);
     });
-  }, [])
+  }, []);
+
+  const addNewTask = async () => {
+    await axios.post('http://localhost:8000/createTask', {
+      text,
+      isCheck: false
+    }).then((res) => {
+      console.log(res.data);
+      // setTasks(res.data.data);
+    });
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>To-Do List</h1>
         <div>
-          <input type="text" />
-          <button>Add</button>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button onClick={() => addNewTask()}>Add</button>
         </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
